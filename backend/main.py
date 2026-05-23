8d9c42b241f3f210c63eafe914d7ef2c823e6590
Mohammed-Shoeb07 <mohdshoaiblateef786@gmail.com>
2026-05-23 00:11:22 -0230

    Allow Netlify frontend origin

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://stellular-bavarois-1c9ed8.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
